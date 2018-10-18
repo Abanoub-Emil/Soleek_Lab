faa652b81fb65f006327e939649b5afa9188f566
Abanoub-Emil <Champion@bakhoums-macbook-pro.local>
2018-10-18 23:41:52 +0200

    Created packages and signIn/signUp view Controllers

---
+++ Soleek Task/View Controller/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//   SignInViewController.swift
+//  Soleek Task
+//
+//  Created by Champion on 10/18/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
